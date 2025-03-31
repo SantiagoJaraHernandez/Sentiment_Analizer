7cb21ab6b2b5ac4e19d451ac05be66b1a0e93c15
SantiagoJaraHernandez <167146795+SantiagoJaraHernandez@users.noreply.github.com>
2025-03-30 20:03:55 -0500

    Added Procfile for Railway deployment

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # 🌍 URL de la API
-API_URL = "http://127.0.0.1:8000"
+API_URL = "sentimentanalizer-production.up.railway.app"
 
 # 🔹 Título del Proyecto
 st.markdown("<h1 style='text-align: center;'>🔍 Análisis de Sentimientos en textos con IA</h1>", unsafe_allow_html=True)
